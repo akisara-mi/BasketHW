1b771f4a59aa00daf2265e33f91ec87b63c7ab4e
akisara-mi <marijavasa88@gmail.com>
2023-01-25 00:36:01 +0100

    HW

---
+++ src/test/java/tests/BasketTest.java
@@ -0,0 +1,42 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class BasketTest extends BaseTest {
+
+    By emailField = By.id("Email");
+    By passwordField = By.id("Password");
+    By loginButton = By.cssSelector("input[value='Log in']");
+    By priceOne = By.xpath("//tbody/tr[1]/td[6]");
+    By priceTwo = By.xpath("//tbody/tr[2]/td[6]");
+    By priceThree = By.xpath("//tbody/tr[3]/td[6]");
+    By totalPrice = By.xpath("//tbody/tr[4]/td[2]");
+
+    @Test
+    public void doTest() {
+
+        typeIn(emailField, "marijavasa88@gmail.com");
+        typeIn(passwordField, "Test123");
+        clickOnElement(loginButton);
+        clickOnElement(shoppingChart);
+
+        String productPrice1 = getTextFromElement(priceOne);
+
+        double productPriceNum1 = Double.valueOf(productPrice1);
+
+        String productPrice2 = getTextFromElement(priceTwo);
+        double productPriceNum2 = Double.valueOf(productPrice2);
+
+        String productPrice3 = getTextFromElement(priceThree);
+        double productPriceNum3 = Double.valueOf(productPrice3);
+
+        String totalPriceOfProduct = getTextFromElement(totalPrice);
+        double totalPriceSum = Double.valueOf(totalPriceOfProduct);
+
+        Double sumProductPrice = productPriceNum1 + productPriceNum2 + productPriceNum3;
+        Assert.assertEquals(sumProductPrice, totalPriceSum);
+    }
+
+}
